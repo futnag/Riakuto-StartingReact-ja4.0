1cdf019c4c32eb1741197b9a44338738c28ff2c7
futnag <futoshinagata@gmail.com>
2022-09-19 19:24:50 +0900

    5 JSX で UI を表現する まで

--- 05-jsx/02-jsx-usage/jsx-demo/src/components/Greet.tsx
+++ 05-jsx/02-jsx-usage/jsx-demo/src/components/Greet.tsx
@@ -1,12 +1,10 @@
 type Props = { name: string; times?: number };
 
-const Greet: React.FC<Props> = (props) => {
-  const { name, times = 1 } = props;
-
+const Greet: React.FC<Props> = ({ name, times = 1 }) => {
   return (
     <>
-      {[...Array(times)].map(() => (
-        <p>Hello, {name}!</p>
+      {[...Array(times).keys()].map((i) => (
+        <p key={i}>Hello, {name}!</p>
       ))}
     </>
   );
